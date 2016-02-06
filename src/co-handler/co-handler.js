9c300bf056fdd0c170abff26802cae02e8c69794
Vlad Aituganov <vlad.aituganov@gmail.com>
2016-02-06 18:06:52 +0300

    Reworked co-handler

--- src/co-handler/co-handler.js
+++ src/co-handler/co-handler.js
@@ -2,33 +2,42 @@ import Promise from 'bluebird';
 import is from 'is';
 import {isPromise} from '../utils';
 import combineYieldTransforms from '../combine';
-import {isGeneratorFunction} from '../utils';
-import runCo from '../co-dispatchable';
+// import {isGeneratorFunction} from '../utils';
+// import runCo from '../co-dispatchable';
+
+import {values} from 'ramda';
+import {Maybe} from 'ramda-fantasy';
+const {Just, Nothing} = Maybe;
 
 export default combineYieldTransforms([
   // wrapPromiseValue,
+  objectToPromise,
+  arrayOfPromises,
   // generatorFunctionToPromise,
-  arrayToPromise,
-  // objectToPromise,
-  x => ({result: true, value: x})
+  x => Just(x)
 ]);
 
-function wrapPromiseValue(x) {
-  if (!isPromise(x)) throw new Error('not a promise');
-  
-  return Promise.resolve( () => x );
+function arrayOfPromises(arr) {
+  if (!isArrayOfPromises(arr)) return Nothing();
+
+  return Just(Promise.all(arr));
 }
 
-function arrayToPromise(arr) {
-  if (!is.array(arr)) throw new Error('not an array');
+function isArrayOfPromises(arr) {
+  if (!is.array(arr)) return false;
 
-  return {
-    result: true, value: Promise.all(arr)
-  };
+  const promisesCount = arr
+    .map(isPromise)
+    .filter(x => (x === true))
+    .length;
+
+  return promisesCount > 0;
 }
 
+
+// TODO refactor
 function objectToPromise(obj) {
-  if (!isObject(obj)) throw new Error('not an object');
+  if (!isObjectWithPromises(obj)) return Nothing();
 
   var results = new obj.constructor();
   var keys = Object.keys(obj);
@@ -42,9 +51,7 @@ function objectToPromise(obj) {
       results[key] = obj[key];
     }
   }
-  return Promise.resolve(() => 
-    (Promise.all(promises).then(() => results))
-  );
+  return Just(Promise.all(promises).then(() => results));
 
   function defer(promiseToDefer, promiseKey) {
     // predefine the key in the result
@@ -55,14 +62,31 @@ function objectToPromise(obj) {
   }
 }
 
-function generatorFunctionToPromise(x) {
-  if (!isGeneratorFunction(x)) throw new Error('not a generator function');
+function isObjectWithPromises(obj) {
+  if (!is.object(obj)) return false;
 
-  return Promise.resolve(() => 
-    runCo(x)
-  );
+  const vals = values(obj);
+  return isArrayOfPromises(vals);
+}
+
+
+/*
+function generatorFunctionToPromise(x) {
+  if (!isGeneratorFunction(x)) return Nothing();
+
+  return Just(runCo(x));
+}
+*/
+
+
+/*
+function wrapPromiseValue(x) {
+  if (!isPromise(x)) throw new Error('not a promise');
+  
+  return Promise.resolve( () => x );
 }
 
 const isObject = val =>  (
   Object == val.constructor
 )
+*/
--- src/co-handler/co-handler.spec.js
+++ src/co-handler/co-handler.spec.js
@@ -2,35 +2,36 @@ require('babel-polyfill');
 import {test} from 'tap';
 import is from 'is';
 import coHandler from './co-handler';
-import run from '..';
 import Promise from 'bluebird';
 import assert from 'assert';
 
-test('co-handler is yield handler = a function, returning a promise', t => {
+// import {Maybe} from 'ramda-fantasy';
+// const {getOrElse, Just, Nothing} = Maybe;
+
+
+test('co-handler is a function', t => {
   t.equal(is.fn(coHandler), true, 'it is a function');
-  t.equal(is.fn(coHandler().then), true, 'it returns a promise');
   t.end();
 });
 
-
-/*
 
 test('co-handler returns a Promise.all call result if called with an array of promises', () => {
   const testValues = [ 1, 2 ];
   const testPromises = testValues.map(Promise.resolve);
 
   return coHandler(testPromises)
+    .getOrElse('Error: expected to return a Just Maybe instance')
     .then(results => {
-      assert.deepEqual(results, testValues, 'co-handler result is equal to the initial array');
+      assert.deepEqual(results, testValues, 'result is equal to the initial array');
       return true;
     })
 });
 
 
-
 test(`co-handler maps the values and resolves the promises in the array`, () => {
   const testingFunction = ([arg, expected]) => (
     coHandler(arg)
+      .getOrElse('Error: expected to return a Just Maybe instance')
       .then(
         result => assert.deepEqual(result, expected, 'expect returned and passed values to match')
       )
@@ -39,17 +40,20 @@ test(`co-handler maps the values and resolves the promises in the array`, () => 
   return Promise.all([
     [ [1, Promise.resolve(2)], [1, 2] ],
     [ [Promise.resolve(1), Promise.resolve(2), ''], [1, 2, ''] ],
-    [ [Promise.resolve(1), 'test'], [1, 'test']],
-    [ [], [] ]
-  ].map(testingFunction));
+    [ [Promise.resolve(1), 'test'], [1, 'test']]
+  ].map(testingFunction))
+  .then( 
+    () => true,
+    () => Promise.reject('one failed')
+  );
 });
-
 
 
 
 test(`co-handler returns a promise which gets rejected if one of the promises in the array were rejected`, () => {
   const testingFunction = ([arg, expected]) => (
     coHandler(arg)
+      .getOrElse('Error: expected to return a Just Maybe instance')
       .then(
         () => { throw 'expected to fail' },
         result => assert.deepEqual(result, expected, 'expect returned and passed values to match')
@@ -68,17 +72,19 @@ test(`co-handler returns a promise which gets rejected if one of the promises in
 test(`co-handler maps the values and resolves the promises in an object`, () => {
   const testingFunction = ([arg, expected]) => (
     coHandler(arg)
+      .getOrElse('Error: expected to return a Just Maybe instance')
       .then( result => assert.deepEqual(result, expected, 'expect returned and passed values to match') )
   );
 
   return Promise.all([
     [ {a: 1, b: Promise.resolve(2)}, {a: 1, b: 2} ],
     [ {a: Promise.resolve(1), b: Promise.resolve(2), c: ''}, {a: 1, b: 2, c: ''} ],
-    [ {a: Promise.resolve(1), b: 'test'}, {a: 1, b: 'test'} ],
-    [ {}, {} ]
+    [ {a: Promise.resolve(1), b: 'test'}, {a: 1, b: 'test'} ]
   ].map(testingFunction));
 });
 
+
+/*
 
 
 test(`co-handler runs the generator function with run() if it is passed`, () => {
@@ -96,23 +102,25 @@ test(`co-handler runs the generator function with run() if it is passed`, () => 
   ].map(testingFunction));
 });
 
+*/
 
+
+
+/*
 test(`co-handler resovles with identity on other types, and plain arrays and objects`, () => {
   const testingFunction = arg => (
     coHandler(arg)
-    .then(result => { assert.deepEqual(result, arg); })
+      .getOrElse('Error: expected to return a Just Maybe instance')
+      .then(result => assert.deepEqual(result, arg))
   );
 
   return Promise.all([
     123,
     'test',
     {a: 1, b: 2},
-    [1, 2, 3],
-    null,
-    undefined
+    [1, 2, 3]
   ].map(testingFunction));
 });
-
 
 test(`co-handler with run() allows generator to try/catch promise errors`, () => {
   const TEST_ERROR = 'boom';
